eb9479afbd433d750969d3429408aea470597ea4
Fernando Guillen <fguillen.mail@gmail.com>
2024-09-12 21:13:08 +0200

    Scaffold

---
+++ lib/mini_lang.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mini_lang/version"
+
+module MiniLang
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mini_lang/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MiniLang
+  VERSION = "0.1.0"
+end
